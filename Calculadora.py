6c1fca413906af467811edf531e68eedde7779e3
Morynasid <guzmanpadillagermanandres546@gmail.com>
2025-05-07 09:28:30 -0500

    Se agrego resta y division

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,13 @@
 a=input("Ingrese el primer numero")
 b=input("ingrese el segundo numero")
 
-operacion=("ingrese el tipo de operacion(1=suma,2=resta,3=multiplicacion,4=division)")
+operacion=input("ingrese el tipo de operacion(1=suma,2=resta,3=multiplicacion,4=division)")
+
+if operacion==1
+
+elif operacion==2
+resultado=a-b
+elif operacion==3
+
+elif operacion==4
+resultado=a/b
