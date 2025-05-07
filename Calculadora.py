301ece749def7d0852c32f5cf6344fe86e2a4857
Kelimporta99 <simonchala99@gmail.com>
2025-05-07 09:29:34 -0500

    se agrego la suma y la multiplicación

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,12 @@
 a=input("Ingrese el primer numero")
 b=input("ingrese el segundo numero")
 
-operacion=("ingrese el tipo de operacion(1=suma,2=resta,3=multiplicacion,4=division)")
+operacion=input("ingrese el tipo de operacion(1=suma,2=resta,3=multiplicacion,4=division)")
+
+if operacion==1:
+    resultado=int(a)+int(b)
+    print("El resultado de la suma es",resultado)
+elif operacion==3:
+    resultado=int(a)*int(b)
+    print("El resultado de la multiplicacion es",resultado)
+break
